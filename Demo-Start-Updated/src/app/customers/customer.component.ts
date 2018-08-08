319a57627e6b8db877027f762476a8b40115bdd0
gabor182 <schmied.gabor@gmail.com>
2018-08-08 16:35:16 +0200

    added a new FormGroup with email confirmation

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -3,6 +3,19 @@ import { FormGroup, Validators, FormBuilder, AbstractControl, ValidatorFn } from
 
 import { Customer } from './customer';
 
+function emailMatcher(c: AbstractControl) {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+  if (emailControl.value !== confirmControl.value) {
+    return { 'match': true };
+  }
+  return null;
+}
+
 function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): {[key: string]: boolean} | null => {
     if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
@@ -27,7 +40,10 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', [Validators.required, Validators.email]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', [Validators.required]],
+      }, { validator: emailMatcher }),
       phone: '',
       notification: 'email',
       sendCatalog: true,
